c7a355a10c67ce402a9bc449531c160e64116b74
ivan <tuchel.ivan@gmail.com>
2021-08-02 17:17:28 +0300

    change animations using aos library

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,52 +1,21 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import beerClassify from '../../assets/images/BEER.png';
 import Header from '../header/header';
 import { SRLWrapper } from 'simple-react-lightbox';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import './main.scss';
 
 const Main = () => {
-  const [showHeaderClassify, setShowHeaderClassify] = useState('');
-  const [showBeerClassify, setShowBeerClassify] = useState('');
-  const [showHeaderDeg, setShowHeaderDeg] = useState('');
-  const [scroll, setScroll] = useState(0);
-
   const [navTransparent, setNavTransparent] = useState('top');
-
-  const refHeaderClassify = useRef();
-  const refBeerClassify = useRef();
-  const refHeaderDeg = useRef();
-
-  const isInViewport = (el) => {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  };
 
   useEffect(() => {
     window.onscroll = () => {
-      /* при первом рендере ref.current === undefined 
-      ref свяжется с дом-элементом только после изменения состояния. 
-      меняем состояние при первом же скролле */
-      setScroll(1);
-
       if (window.pageYOffset > 100) {
         setNavTransparent('');
       } else setNavTransparent('top');
 
-      if (scroll && isInViewport(refHeaderClassify.current)) {
-        setShowHeaderClassify('show');
-      }
-      if (scroll && isInViewport(refBeerClassify.current)) {
-        setShowBeerClassify('show');
-      }
-      if (scroll && isInViewport(refHeaderDeg.current)) {
-        setShowHeaderDeg('show');
-      }
+      Aos.init({ duration: 1500 });
     };
   });
 
@@ -55,21 +24,18 @@ const Main = () => {
       <Header top={navTransparent} />
       <section id="home">
         <div className="container content">
-          <div
-            ref={refHeaderClassify}
-            className={`home-heading ${showHeaderClassify}`}
-          >
+          <div data-aos="fade-up" className="home-heading">
             <h1 className="bottom-line">Классификация сортов пива</h1>
           </div>
           <SRLWrapper>
-            <div ref={refBeerClassify} className={`images ${showBeerClassify}`}>
+            <div data-aos="fade-up" className="images">
               <img src={beerClassify} alt="Классификация пива" />
             </div>
           </SRLWrapper>
 
           <div className="line2"></div>
 
-          <div ref={refHeaderDeg} className={`home-heading ${showHeaderDeg}`}>
+          <div data-aos="fade-up" className="home-heading">
             <h1 className="bottom-line">Последние дегустации</h1>
           </div>
         </div>
